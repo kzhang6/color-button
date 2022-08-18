e30256aa068619d62cbfdd00c27045badd88607a
Kristine Zhang <kristine.zhang@cognosante.com>
2022-08-17 22:59:09 -0700

    roles?

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<App />);  //create virtual DOM
+  // const linkElement = screen.getByText(/learn react/i); // Access virtual DOM
+  const linkElement = screen.getByRole('link', {name: /learn react/i}); // a tag <a> has a role of link?
   expect(linkElement).toBeInTheDocument();
 });
